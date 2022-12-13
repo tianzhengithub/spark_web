7e9997a424d347ae86b4d007dd7bf17c0e32f0ca
tianzhenGitHUb <tianzhenboston@gmail.com>
2022-12-13 22:19:14 +0800

    maven plugin

--- spark_web/src/main/java/org/example/tianzhen/Main.java
+++ spark_web/src/main/java/org/example/tianzhen/Main.java
@@ -1,9 +1,21 @@
 package org.example.tianzhen;
-
+import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "hello spark web ");
+
+        final String[] tempStr = {""};
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            String temp = scanner.next();
+            if (temp.equals("end")){
+                break;
+            }
+            tempStr[0] = temp;
+            System.out.println("最新结果是：" + tempStr[0]);
+        }
+        get("/hello", (req, res) -> tempStr[0]);
     }
 }
