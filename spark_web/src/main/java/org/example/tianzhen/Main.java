074239da7ba433dbd67f102c6a6615e5e1289d35
tianzhengithub <tianzhenboston@gmail.com>
2023-01-08 10:29:37 +0000

    code check

--- spark_web/src/main/java/org/example/tianzhen/Main.java
+++ spark_web/src/main/java/org/example/tianzhen/Main.java
@@ -1,7 +1,5 @@
 package org.example.tianzhen;
-import org.w3c.dom.ls.LSOutput;
-import java.util.Scanner;
-import static spark.Spark.*;
+import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
