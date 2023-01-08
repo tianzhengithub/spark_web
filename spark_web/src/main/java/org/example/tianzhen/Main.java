0243cf30199740302789ef4200c7c9b91feaa2db
tianzhengithub <tianzhenboston@gmail.com>
2023-01-08 10:36:13 +0000

    add Test txt

--- spark_web/src/main/java/org/example/tianzhen/Main.java
+++ spark_web/src/main/java/org/example/tianzhen/Main.java
@@ -3,6 +3,6 @@ import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "hello sparkWeb");
+        get("/hello", (req, res) -> "hello sparkWeb 111");
     }
 }
