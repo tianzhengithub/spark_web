c5ef93553ed7fcd9c01c389934d274ab800f4e1b
tianzhenGitHUb <tianzhenboston@gmail.com>
2023-01-08 09:47:15 +0000

    add dockerFile

--- spark_web/src/main/java/org/example/tianzhen/Main.java
+++ spark_web/src/main/java/org/example/tianzhen/Main.java
@@ -5,17 +5,6 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        final String[] tempStr = {""};
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()){
-            String temp = scanner.next();
-            if (temp.equals("end")){
-                break;
-            }
-            tempStr[0] = temp;
-            System.out.println("最新结果是：" + tempStr[0]);
-        }
-        get("/hello", (req, res) -> tempStr[0]);
+        get("/hello", (req, res) -> "hello sparkWeb");
     }
 }
